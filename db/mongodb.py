2c13b02dabbcd109cb3f583c4bc97d06e178db66
Menelik_Berhan <MenelikBerhan@gmail.com>
2024-03-21 01:18:56 +0300

    Add a database client to manage connections to MongoDb

--- db/mongodb.py
+++ db/mongodb.py
@@ -1,2 +1,25 @@
 """Mongodb database client
 """
+from config.setup import settings
+from motor.motor_asyncio import AsyncIOMotorClient
+
+
+class DbClient:
+    """A mongodb database connection client."""
+    def __init__(self):
+        """Initialize instance"""
+        self.url = f'mongodb://{settings.DB_HOST}:{settings.DB_PORT}'
+
+    async def connect(self):
+        """Establishes mongodb connection on app startup."""
+        print("Connecting to mongodb database")
+        # create a mongodb client & database
+        self.client = AsyncIOMotorClient(self.url)
+        self.db = self.client[settings.DB_NAME]
+
+    async def disconnect(self):
+        """Closes mongodb connection on app shutdown."""
+        print("Closing connection to mongodb database")
+        self.client.close()
+
+db_client = DbClient()

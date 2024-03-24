e7477dd5a880e7325566dd70acb2094050e436c7
Menelik_Berhan <MenelikBerhan@gmail.com>
2024-03-24 17:20:17 +0300

    Add a feature to retrieve image metadata & write image to local storage using a background task
    Processing the image is done in the background. Response to POST /image is send immediately before retrieving image metadata & writing image to local storage .

--- utils/file_read_write.py
+++ utils/file_read_write.py
@@ -1,10 +1,12 @@
 """Utilities for file reading & writing operations
 """
-from fastapi import UploadFile
-from uuid import uuid4
+from bson import ObjectId
 from config.setup import settings
+from db.mongodb import db_client
+from fastapi import UploadFile
 from os import path, mkdir
 from PIL import Image
+from uuid import uuid4
 import io
 
 
@@ -53,3 +55,39 @@ async def write_file(file: UploadFile) -> str:
     # await file.close()
 
     return image_dict
+
+
+async def background_write_file(file_buffer: bytes, file_name: str, id: str):
+    """Retrieves image metadata from buffer and saves image to local storage.
+    """
+    storage_path = settings.STORAGE_PATH
+    # check if storage path exists & is a directory. If not create one.
+    if not (path.isdir(storage_path) or path.exists(storage_path)):
+        print(f"Directory doesn't exist at given path: '{storage_path}'")
+        mkdir(storage_path)
+        print(f"Created storage directory: '{storage_path}")
+
+    # set absolute local storage path by appending file name to storage path
+    file_path = path.join(path.abspath(storage_path), file_name)
+
+    # load image from bytes buffer using Pillow & BytesIO
+    image = Image.open(io.BytesIO(file_buffer))
+
+    # remove unnecessary info that causes errors for large png files
+    info = image.info
+    info.pop('icc_profile', None)
+
+    # get image metadata and update image in db with it
+    image_dict = {
+        "local_path": file_path,
+        "image_size": image.size,
+        "image_format": image.format,
+        "image_mode": image.mode,
+        "info": info,
+        }
+
+    await db_client.db['images'].update_one({'_id': ObjectId(id)}, {'$set': image_dict})
+
+    # save image to local storage. then close file pointer to image 
+    image.save(file_path)
+    image.close()

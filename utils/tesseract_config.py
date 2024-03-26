52fe2ba7571270d06ff6c8dd26bdbfb2f039e70e
Menelik_Berhan <MenelikBerhan@gmail.com>
2024-03-26 20:30:53 +0300

    Fix pycodestyle & type check errors

--- utils/tesseract_config.py
+++ utils/tesseract_config.py
@@ -9,16 +9,16 @@ async def background_setup_tess_config(tess_req_dict: dict) -> dict:
     preapares a TesseractConfigurationModel, and return it as dict."""
 
     # check if a TesseractConfigModel already exist in db for given params
-    config_dict = await db_client.db['tess_config'].find_one(tess_req_dict)
+    config_dict = await db_client.db.tess_config.find_one(tess_req_dict)
 
     # if no TesseractConfigModel exist in db, create one
     if config_dict is None:
         tess_config = TesseractConfigModel(**tess_req_dict)
         # dump model to dict excluding `id` (None)
-        config_dict = tess_config.model_dump(exclude=['id'])
+        config_dict = tess_config.model_dump(exclude={'id'})
 
         # insert in db & assign id to config dict
-        insert_result = await db_client.db['tess_config'].insert_one(config_dict)
+        insert_result = await db_client.db.tess_config.insert_one(config_dict)
         config_dict['id'] = insert_result.inserted_id
     else:
         # if returned from db, set `id`

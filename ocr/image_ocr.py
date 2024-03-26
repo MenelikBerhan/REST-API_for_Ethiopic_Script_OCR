dce6f2e1f0a4725b89655727e4ba830946ab7c13
Menelik_Berhan <MenelikBerhan@gmail.com>
2024-03-27 00:59:05 +0300

    Run tesseract OCR in custom process pool since it is a CPU-bound operation
    Based on: [REFERENCE](https://docs.python.org/3.8/library/asyncio-eventloop.html#asyncio.loop.run_in_executor)

--- ocr/image_ocr.py
+++ ocr/image_ocr.py
@@ -1,49 +1,78 @@
 """Image OCR using pytesseract
 """
 from bson import ObjectId
+from datetime import datetime, timezone
 from db.mongodb import db_client
-from models.images import ImageModel
 from utils.file_read_write import background_write_file
 from utils.tesseract_config import background_setup_tess_config
-import pytesseract as pts
 import asyncio
-import cv2
+import concurrent.futures
+import pytesseract as pts
 
 
+def ocr_image(image):
+    """
+    Synchronous OCR by tesseract, used in custom process pool.
+    Needs to be global to use process pool.
+    """
+    return pts.image_to_string(image, lang='amh-old')
 
-async def background_image_ocr(file_buffer: bytes, file_name: str, id: str, tess_req_dict: dict):
+
+async def background_image_ocr(
+        file_buffer: bytes, file_name: str, id: str, tess_req_dict: dict):
     """Performs OCR on image using background tasks.
 
     Args:
         file_buffer (bytes): uploaded image as bytes
         file_name (str): name of uploaded image
         id (str): id of image in database
-        tess_req_dict (dict): tesseract configuration parameters passed in request body
+        tess_req_dict (dict): tesseract configuration parameters passed
+            in request body
     """
-    # await asyncio.sleep(20) 
-    # get tesseract configuration based on given params
+    # get tesseract configuration based on given params in request
     config_dict = await background_setup_tess_config(tess_req_dict)
-    # print('CONFIGURED')
     print('Tesseract: CONFIGURED')
-    # save image in local storage & update image in db with metadata & storage path
-    await background_write_file(file_buffer, file_name, id, config_dict['id'])
+
+    # save image in local storage & get image_dict (metadata & storage path)
+    image, image_dict = await background_write_file(
+        file_buffer, file_name, id)
     print('Image: WRITTEN 2 LOCAL')
 
-    image_db: ImageModel = await db_client.db.images.find_one({'_id': ObjectId(id)})
-
-    image = cv2.imread(image_db['local_path'])
-    
-    print('Image: LOADED by CV2')
-
+    # [REFERENCE](https://docs.python.org/3.8/library/asyncio-eventloop.html#asyncio.loop.run_in_executor)  # noqa
+    # get the running event loop in the current OS thread
     loop = asyncio.get_running_loop()
-    ocr_result = await loop.run_in_executor(None, lambda: pts.image_to_string(image, lang='amh-old'))
+    tasks_before = asyncio.all_tasks()
+    while len(tasks_before) > 3:
+        await asyncio.sleep(5)
+        tasks_before = asyncio.all_tasks()
 
-    print('Tesseract: OCR FINISHED')
+    # print(f'BEFORE: {len(tasks_before)} tasks: {[(t.get_name()) for t in tasks_before]}') # noqa
 
-    # ocr_result = pts.image_to_string(image, lang='amh-old')
-    # print(ocr_result)
-    # image_db['ocr_finished'] = True
-    # image_db['ocr_result'] = ocr_result
+    # # 1. Run tesseract OCR in the default loop's executor
+    # ocr_result = await loop.run_in_executor(
+    #     None, lambda: pts.image_to_string(image, lang='amh-old'))
+
+    # # 2. Run tesseract OCR in a custom thread pool:
+    # with concurrent.futures.ThreadPoolExecutor() as pool:
+    #     ocr_result = await loop.run_in_executor(
+    #         pool, lambda: pts.image_to_string(image, lang='amh-old'))
+    #     print(ocr_result)
+
+    # 3. Run in a custom process pool:
+    with concurrent.futures.ProcessPoolExecutor() as pool:
+        ocr_result = await loop.run_in_executor(
+            pool, ocr_image, image)
+        print('Tesseract: OCR FINISHED')
+
+    # tasks_after = asyncio.all_tasks()
+    # print(f'AFTER: {len(tasks_after)} tasks: {[(t.get_name()) for t in tasks_after]}') # noqa
+
+    # update image in db
+    image_dict.update({
+        'ocr_finished': True,
+        'ocr_result': ocr_result,
+        "tess_config_id": config_dict['id'],
+        "updated_at": datetime.now(timezone.utc)})
 
     await db_client.db.images.update_one(
-        {'_id': ObjectId(id)}, {'$set': {'ocr_result': ocr_result}})
+        {'_id': ObjectId(id)}, {'$set': image_dict})

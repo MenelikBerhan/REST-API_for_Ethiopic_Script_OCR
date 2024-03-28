d626cc68e18569a872c04a22c9fcd7c430e2511c
Menelik_Berhan <MenelikBerhan@gmail.com>
2024-03-29 00:20:26 +0300

    Add output_format field to TessractConfig model & make doc more visible in swagger UI

--- models/tesseract.py
+++ models/tesseract.py
@@ -8,8 +8,9 @@ from typing import Union, Dict
 import json
 
 
+# TODO: use Field for fields & add field desctiptions
 class Language(str, Enum):
-    """Available OCR languages"""
+    """### Available OCR languages"""
     amharic = 'amh'
     amharic_old = 'amh-old'
     """Fine tuned amharic model (using old prints)"""
@@ -17,41 +18,70 @@ class Language(str, Enum):
     tigrigna = 'tig'
 
 
+class OcrOutputFormat(str, Enum):
+    """### OCR result output formats.
+
+    ### By default OCR result is saved in string form. If the result is to be\
+    saved in file, one of `txt`, `docx` or `pdf` must be passed. After\
+    uploading the image one can also use the `GET /ocr/{image_id}/` endpoint\
+    to get result in other formats."""
+
+    string = 'str'
+    """Default mode."""
+    text = 'txt'
+    """For plain text file"""
+    mswrod = 'docx'
+    """For Microsoft Word file."""
+    pdf = 'pdf'
+    """For pdf file."""
+
+
 class TesseractConfigRequestModel(BaseModel):
     """
     ### An Optional Request body of `POST /images/` for tesseract configuration.
 
-    ### A serialized dictionary (`str`:`str`) of tesseract config parameters.
+    ### A serialized dictionary (`str`:`str`) of tesseract config parameters.\
+    (All fields are optional)
     
     ### Refer [TESSERACT(1) Manual Page](https://github.com/tesseract-ocr/tesseract/blob/main/doc/tesseract.1.asc#options)
 
     ### Check [tesseract_parameters](https://github.com/MenelikBerhan/REST-API_for_Ethiopic_Script_OCR/blob/image_ocr/utils/default_tesseract_parameters)\
     file for list of configurable tesseract variables to use for `config_vars`.
     """  # noqa
+    output_format: OcrOutputFormat = Field(
+        default=OcrOutputFormat.string,
+        description="""__OCR output file format. By default OCR result is saved
+        in string form. If the result is to be saved in file, one of `txt`,
+        `docx` or `pdf` must be passed. After uploading the image use the
+        `GET /ocr/{image_id}/` endpoint to get result in other formats.__
+        """
+    )
+
     language: Language = Field(
         default=Language.amharic_old,
-        description='Language of text in image.')
+        description='__Language of text in image.__')
 
     oem: int = Field(
         default=1, ge=0, le=3,
-        description='OCR engine mode used by tesserat.')
+        description='__OCR engine mode used by tesserat.__')
 
     psm: int = Field(
         default=3, ge=0, le=13,
-        description='Page segmentation mode used by tesseract.')
+        description='__Page segmentation mode used by tesseract.__')
 
     # additional custom configuration flags
     # TODO: validate key names by cross checking with tesseract_parameters file
     config_vars: Dict[str, Union[int, float]] = Field(
         default={},
-        description="`Key: value` pairs of tesseract config variables \
-            (`CONFIGVAR`). Passed to tesseract using `-c`."
+        description="__`Key: value` pairs of tesseract config variables \
+            (`CONFIGVAR`). Passed to tesseract using multiple `-c`.__"
     )
 
     # add config
     model_config = ConfigDict(
         json_schema_extra={
             'example': {
+                'output_format': 'str',
                 'language': 'amh-old',
                 'oem': 1,
                 'psm': 3,
@@ -78,7 +108,7 @@ class TesseractConfigRequestModel(BaseModel):
 
 class TesseractConfigModel(APIBaseModel, TesseractConfigRequestModel):
     """
-    A model class for abstraction of tesseract configuration.
+    ### A model class for abstraction of tesseract configuration.
     """
 
     # add config
@@ -88,7 +118,8 @@ class TesseractConfigModel(APIBaseModel, TesseractConfigRequestModel):
                 'id': '65fb7cc253b139befea1205c',
                 'created_at': '2024-03-21T00:18:10.836000',
                 'updated_at': '2024-03-21T00:18:10.836000',
-                'language': 'amh',
+                'output_format': 'str',
+                'language': 'amh-old',
                 'oem': 1,
                 'psm': 3,
                 'config_vars': {

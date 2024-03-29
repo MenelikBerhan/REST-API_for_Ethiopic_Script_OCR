403173671f299fe876b8c6edc1fe8e806bcfa566
Menelik_Berhan <MenelikBerhan@gmail.com>
2024-03-30 02:23:36 +0300

    Create a TesseractOutputModel object for each image OCR result, and set its id in images models.

--- ocr/image_ocr.py
+++ ocr/image_ocr.py
@@ -9,36 +9,37 @@ from utils.tesseract import background_setup_tess_config,\
 
 
 async def background_image_ocr(
-        file_buffer: bytes, file_name: str, id: str, tess_req_dict: dict):
+        file_buffer: bytes, file_name: str, image_id: str, tess_req_dict: dict
+        ):
     """Performs OCR on image using background tasks.
 
     Args:
         file_buffer (bytes): uploaded image as bytes
         file_name (str): name of uploaded image
-        id (str): id of image in database
+        image_id (str): id of image in database
         tess_req_dict (dict): tesseract configuration parameters passed
             in request body
     """
     # get tesseract configuration based on given params in request
-    config_dict = await background_setup_tess_config(tess_req_dict)
+    tess_config_dict = await background_setup_tess_config(tess_req_dict)
     print('Tesseract: CONFIGURED')
 
     # save image in local storage & get image_dict (metadata & storage path)
     image, image_dict = await background_write_file(file_buffer, file_name)
     print('Image: WRITTEN 2 LOCAL')
 
-    # run tesseract in background & get result in string & dictionary formats
-    ocr_result_dict, ocr_result_text = await background_run_tesseract(
-        image, config_dict)
+    # run tesseract in background & get output model dict & result text
+    tess_output_dict, ocr_result_text = await background_run_tesseract(
+        image, image_id ,tess_config_dict)
     print('Tesseract: OCR FINISHED')
 
     # update image in db
     image_dict.update({
         'ocr_finished': True,
         'ocr_result_text': ocr_result_text,
-        'ocr_result_dict': ocr_result_dict,
-        "tess_config_id": config_dict['id'],
+        'tess_output_id': tess_output_dict['id'],
+        "tess_config_id": tess_config_dict['id'],
         "updated_at": datetime.now(timezone.utc)})
 
     await db_client.db.images.update_one(
-        {'_id': ObjectId(id)}, {'$set': image_dict})
+        {'_id': ObjectId(image_id)}, {'$set': image_dict})
--- utils/tesseract.py
+++ utils/tesseract.py
@@ -2,11 +2,13 @@
 """
 import asyncio
 import pytesseract as pts
+from bson import ObjectId
 from db.mongodb import db_client
 from concurrent.futures import ThreadPoolExecutor
 from config.setup import settings
-from models.tesseract import TesseractConfigModel
+from models.tesseract import TesseractConfigModel, TesseractOutputModel
 from PIL import Image
+import time
 
 
 async def background_setup_tess_config(tess_req_dict: dict) -> dict:
@@ -56,21 +58,24 @@ OCR_IN_PROGRESS: int = 0
 """Keeps track of no. of images being processed"""
 
 
-async def background_run_tesseract(image: Image, config_dict: dict):
+async def background_run_tesseract(image: Image, image_id: str, tess_config_dict: dict):
     """Runs tesseract in a ThreadPool and returns the result.
 
     Args:
         image (Image): image loaded using PIL
-        config_dict (dict): tesseract configuration dictionary
+        image_id (str): id of image in database
+        tess_config_dict (dict): tesseract configuration dictionary
     """
     # set as global (otherwise UnboundLocalError: referenced before assignment)
     global OCR_IN_PROGRESS
 
     # set tesseract config string from config_dict
     config = '-l {} --psm {} --oem {}'.format(
-        config_dict['language'], config_dict['psm'], config_dict['oem'])
+        tess_config_dict['language'],
+        tess_config_dict['psm'],
+        tess_config_dict['oem'])
 
-    for key, value in config_dict['config_vars'].items():
+    for key, value in tess_config_dict['config_vars'].items():
         config += ' -c {}={}'.format(key, value)
 
     # get the running event loop in the current OS thread
@@ -87,12 +92,18 @@ async def background_run_tesseract(image: Image, config_dict: dict):
 
         OCR_IN_PROGRESS = OCR_IN_PROGRESS + 1
 
+        # set start benchmark time
+        start = time.perf_counter()
+
         ocr_result_dict = await loop.run_in_executor(
             pool,
             lambda: pts.image_to_data(
                 image, config=config, output_type=pts.Output.DICT
                 )
             )
+
+        # set time taken (TODO: use other counters & check d/ce)
+        time_taken = time.perf_counter() - start
 
         image.close()   # as precaution (side effect not clear if not closed)
 
@@ -101,4 +112,20 @@ async def background_run_tesseract(image: Image, config_dict: dict):
         # TODO: extract text from dict in a separate function
         ocr_result_text = " ".join(ocr_result_dict['text'])
 
-    return ocr_result_dict, ocr_result_text
+    # create a TesseractOutputModel for this image OCR    
+    tess_output_dict = {
+        'image_id': image_id,
+        'tess_config_id': tess_config_dict['id'],
+        'time_taken': time_taken,
+        'ocr_result_dict': ocr_result_dict,
+    }
+
+    tess_output = TesseractOutputModel(**tess_output_dict)
+
+    # save output in db
+    tess_output_dict = tess_output.model_dump(exclude={'id'})
+    insert_result = await db_client.db.tess_output.insert_one(tess_output_dict)
+
+    tess_output_dict['id'] = insert_result.inserted_id
+
+    return tess_output_dict, ocr_result_text

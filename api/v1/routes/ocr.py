b80c628239eff884789e23a02de9fd2d91187b24
Menelik_Berhan <MenelikBerhan@gmail.com>
2024-04-02 03:00:56 +0300

    Add a feature to write OCR outputs to new file formats added through the `GET /ocr/image/` endpoint

--- api/v1/routes/ocr.py
+++ api/v1/routes/ocr.py
@@ -2,12 +2,14 @@
 """
 from bson import ObjectId
 from bson.errors import InvalidId
+from datetime import datetime, timezone
 from db.mongodb import db_client
 from fastapi import APIRouter, HTTPException, status, Query
 from fastapi.responses import FileResponse, JSONResponse
 from models.images import OcrOutputFormat
 from os import path
 from typing import List
+from utils.file_read_write import background_write_ocr_result
 
 
 # create a router with `/images` prefix
@@ -40,9 +42,10 @@ a 12-byte input or a 24-character hex string""")
 async def get_ocr_output_file_formats(image_id: str) -> List[OcrOutputFormat]:
     """
     ### List of file formats the OCR result is already saved in.<br>\
-    Output file is readily available for file formats in this list.
+    Output file is readily available for file formats in the response list.\
+    <br>If the response list is empty, it means *OCR process is not finished*.
 
-    ### To add formats not in response list use GET `/image/ocr/{image_id}/`\
+    ### To add formats not in response list use GET `/ocr/image/{image_id}/`\
     endpoint.
     """
     # retrieve image from db
@@ -80,14 +83,47 @@ async def get_ocr_result(
         return JSONResponse(image['ocr_result_text'])
 
     if format not in image['done_output_formats']:
-        if add_format:      # create output file in missing format
-            return JSONResponse(f"Creating output file for '{format}' format.")
-
-        raise HTTPException(
-            status.HTTP_404_NOT_FOUND,
-            f"Given format '{format}' not in image's `ocr_output_formats` \
+        if not add_format:
+            raise HTTPException(
+                status.HTTP_404_NOT_FOUND,
+                f"Given format '{format}' not in image's `ocr_output_formats` \
 list. To add it to the list and get the result set `add_format` \
 to `True`.")
+
+        # create output file for missing format
+        # add format to list of output formats (not saved in db)
+        image['ocr_output_formats'].append(format)
+
+        # write ocr output files & get their path (in returned dict)
+        write_ocr_result_dict = await background_write_ocr_result(
+            image['local_path'], image,
+            {'ocr_result_text': image['ocr_result_text']}
+        )
+        print(
+            f"Result Saved in {list(write_ocr_result_dict.keys())} format."
+            )
+
+        # update `done_output_formats` dict in image (not saved in db)
+        image['done_output_formats'].update(write_ocr_result_dict)
+
+        # create dict for updating image in db
+        image_update_dict = {}
+
+        # set images `ocr_output_formats` list and `done_output_formats`
+        # in image_update_dict to save the added formats.
+        image_update_dict['ocr_output_formats'] = image[
+            'ocr_output_formats']
+
+        image_update_dict['done_output_formats'] = image[
+            'done_output_formats']
+
+        # set update_at field
+        image_update_dict['updated_at'] = datetime.now(timezone.utc)
+        # update image in db
+        await db_client.db.images.update_one(
+            {'_id': image['_id']},
+            {'$set': image_update_dict},
+            )
 
     # set output file name from image name
     base_name, _ = path.splitext(image['name'])

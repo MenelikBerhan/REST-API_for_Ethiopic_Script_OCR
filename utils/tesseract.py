cd531c4d862657de04a110d4f6e392bb395476ae
Menelik_Berhan <MenelikBerhan@gmail.com>
2024-04-02 23:23:55 +0300

    Add a function to run tesseract in the background for pdf files

--- utils/tesseract.py
+++ utils/tesseract.py
@@ -2,13 +2,13 @@
 """
 import asyncio
 import pytesseract as pts
-from bson import ObjectId
 from db.mongodb import db_client
 from concurrent.futures import ThreadPoolExecutor
 from config.setup import settings
-from models.tesseract import TesseractConfigModel, TesseractOutputModel
+from models.tesseract import TesseractConfigModel, TesseractOutputModel,\
+    TesseractOutputModelPdf
 from PIL.Image import Image
-from typing import Tuple
+from typing import List, Tuple
 import time
 
 
@@ -103,7 +103,7 @@ OCR_IN_PROGRESS: int = 0
 """Keeps track of no. of images being processed"""
 
 
-async def background_run_tesseract(
+async def background_run_tesseract_image(
         image: Image, image_id: str, tess_config_dict: dict):
     """
     Runs tesseract in a ThreadPool and returns the result.
@@ -167,12 +167,105 @@ async def background_run_tesseract(
         'image_id': image_id,
         'tess_config_id': tess_config_dict['id'],
         'time_taken': time_taken,
+        'ocr_accuracy': ocr_accuracy,
         'ocr_result_text': ocr_result_text,
         'ocr_result_dict': ocr_result_dict,
-        'ocr_accuracy': ocr_accuracy
     }
 
     tess_output = TesseractOutputModel(**tess_output_dict)
+
+    # save output model in db
+    tess_output_dict = tess_output.model_dump(exclude={'id'})
+    insert_result = await db_client.db.tess_output.insert_one(tess_output_dict)
+
+    tess_output_dict['id'] = insert_result.inserted_id
+
+    return tess_output_dict
+
+
+async def background_run_tesseract_pdf(
+        pdf_images: List[Image], pdf_id: str, tess_config_dict: dict):
+    """
+    Runs tesseract in a ThreadPool and returns the result.
+
+    Args:
+        pdf_images (List[Image]): list of pdf pages loaded as PIL image.
+        pdf_id (str): id of pdf in database
+        tess_config_dict (dict): tesseract configuration dictionary
+
+    Returns:
+        (dict): a dictionary with information about ocr process and result.
+    """
+    # set as global (otherwise UnboundLocalError: referenced before assignment)
+    global OCR_IN_PROGRESS
+
+    # set tesseract config string from config_dict
+    config = '-l {} --psm {} --oem {}'.format(
+        tess_config_dict['language'],
+        tess_config_dict['psm'],
+        tess_config_dict['oem'])
+
+    for key, value in tess_config_dict['config_vars'].items():
+        config += ' -c {}={}'.format(key, value)
+
+    # get the running event loop in the current OS thread
+    # [REFERENCE](https://docs.python.org/3.8/library/asyncio-eventloop.html#asyncio.loop.run_in_executor)  # noqa
+    loop = asyncio.get_running_loop()
+
+    # run tesseract OCR in a custom thread pool. Increase workers if available.
+    # (Better efficieny than ProcessPoolExecutor)
+    with ThreadPoolExecutor(max_workers=settings.OCR_WORKERS) as pool:
+        # a dictionary to store ocr results for each page
+        pages_ocr_result = {
+            'time_taken': {}, 'ocr_result_text': {},
+            'ocr_result_dict': {}, 'ocr_accuracy': {}
+            }
+
+        for i, image in enumerate(pdf_images):
+            # before running OCR on a new image, wait for previous one to finish.
+            # if added immediately, executor won't return until all images are done
+            while OCR_IN_PROGRESS > 0:
+                await asyncio.sleep(2)  # TODO: check d/t sleep durations effect
+
+            OCR_IN_PROGRESS = OCR_IN_PROGRESS + 1
+
+            # set start benchmark time
+            start = time.perf_counter()
+
+            ocr_result_dict = await loop.run_in_executor(
+                pool,
+                lambda: pts.image_to_data(
+                    image, config=config, output_type=pts.Output.DICT
+                    )
+                )
+
+            # set time taken (TODO: use other counters & check d/ce)
+            time_taken = time.perf_counter() - start
+
+            image.close()   # as precaution (side effect not clear if not closed)
+
+            OCR_IN_PROGRESS = OCR_IN_PROGRESS - 1
+
+            # parse result dict and get text and accuracy
+            ocr_result_text, ocr_accuracy = parse_ocr_result(ocr_result_dict)
+
+            # store each pages result
+            pages_ocr_result['time_taken'][str(i + 1)] = time_taken
+            pages_ocr_result['ocr_accuracy'][str(i + 1)] = ocr_accuracy
+            pages_ocr_result['ocr_result_text'][str(i + 1)] = ocr_result_text
+            pages_ocr_result['ocr_result_dict'][str(i + 1)] = ocr_result_dict
+
+    # create a TesseractOutputModelPdf for this pdf OCR
+    pdf_tess_output_dict = {
+        'pdf_id': pdf_id,
+        'tess_config_id': tess_config_dict['id'],
+        'time_taken': pages_ocr_result['time_taken'],
+        'ocr_accuracy': pages_ocr_result['ocr_accuracy'],
+        'ocr_result_text': pages_ocr_result['ocr_result_text'],
+        'ocr_result_dict': pages_ocr_result['ocr_result_dict'],
+    }
+
+    tess_output = TesseractOutputModelPdf(**pdf_tess_output_dict)
 
     # save output model in db
     tess_output_dict = tess_output.model_dump(exclude={'id'})
